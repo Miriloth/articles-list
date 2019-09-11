060e4f821189b72af60a8585babe0f2feb98d129
Michał Grabowski <Miriloth@gmail.com>
2019-09-11 22:24:21 +0200

    Enable sorting by date by its value

--- src/scenes/articles/utils/sortArticlesByDate.js
+++ src/scenes/articles/utils/sortArticlesByDate.js
@@ -1,11 +1,34 @@
 import map from 'lodash/map';
-import orderBy from 'lodash/orderBy';
+import sortBy from 'lodash/sortBy';
+import { sortingOptions } from './sortingOptions';
+
+const incorrectMonths = {
+  oktober: 'oktober',
+  mai: 'mai',
+  desember: 'desember',
+};
+const correctMonths = {
+  [incorrectMonths.oktober]: 'october',
+  [incorrectMonths.mai]: 'may',
+  [incorrectMonths.desember]: 'december',
+};
+
+const correctDate = (dateString) => Object.values(incorrectMonths)
+  .reduce((result, incorrectMonth) => (
+    dateString.indexOf(incorrectMonth) > -1
+      ? dateString.replace(incorrectMonth, correctMonths[incorrectMonth])
+      : result
+  ), dateString);
 
 const sortArticlesByDate = (entries, sortingOption) => {
   const articles = map(entries, (articleEntry) => articleEntry);
 
-  return orderBy(articles, ['date'], [sortingOption])
+  const sortedArray = sortBy(articles, [
+    (article) => new Date(correctDate(article.date)).getTime(),
+  ])
     .map((article) => article.id);
+
+  return sortingOption === sortingOptions.DESC ? sortedArray : sortedArray.reverse();
 };
 
 export { sortArticlesByDate };
